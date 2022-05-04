703d0aaeaeacde2e4184ad44952ceb0c8c768ad7
Dastadead <deadasta@gmail.com>
2022-05-03 20:03:56 -0400

    (Modificando vistas) Añadida carpeta template y archivos index.html, contacto.html

--- run.py
+++ run.py
@@ -1,1 +1,28 @@
-git
+#Importar
+from flask import Flask
+
+#Crear app medante instancia
+app = Flask(__name__)
+
+#Crear rutas
+@app.route('/', methods=['GET'])  #Indicamos metodo GET
+def holamundo():
+    return 'Hola Mundo!'
+
+
+@app.route('/sobremi', methods=['GET'])
+def sobremi():
+    return 'Aquí se mostrarán mis proyectos'
+
+
+
+
+
+
+
+
+#Ejecutar nuestra app con run.py
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
